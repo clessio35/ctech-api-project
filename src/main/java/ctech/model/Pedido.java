30d66921fd66401f4865146011dd62afd4d9556b
Clessio <clessio.silva1989@gmail.com>
2025-09-08 09:44:58 -0300

    model pedido

--- src/main/java/ctech/model/Pedido.java
+++ src/main/java/ctech/model/Pedido.java
@@ -1,5 +1,43 @@
 package ctech.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="pedidos")
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descricao;
+	private BigDecimal valor;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+	
+	
 }
