f5622e39ac307a8cf206200c9a3bbafecd4fb3b4
Clessio <clessio.silva1989@gmail.com>
2025-09-08 09:47:24 -0300

    DTO pedido

---
+++ src/main/java/ctech/dto/PedidoDTO.java
@@ -0,0 +1,26 @@
+package ctech.dto;
+
+import java.math.BigDecimal;
+
+public class PedidoDTO {
+
+	private String descricao;
+	private BigDecimal valor;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+}
